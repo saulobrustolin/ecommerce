a93d5380de0f3df85dbd8ed2ae4594aab4004822
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-08-27 10:16:00 -0300

    fix migration create_reviews_table

--- database/migrations/2025_08_27_023946_create_reviews_table.php
+++ database/migrations/2025_08_27_023946_create_reviews_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('reviews', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->longText('describe');
             $table->float('star');
             $table->timestamps();
